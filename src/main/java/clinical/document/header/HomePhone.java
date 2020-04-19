c1d4b4a8e7a559776eb8c9f18a2914863445d50d
Dmitri Bontoft <dmitribontoft@hotmail.com>
2020-04-19 21:04:16 +0100

    Add HomePhone

---
+++ src/main/java/clinical/document/header/HomePhone.java
@@ -0,0 +1,13 @@
+package clinical.document.header;
+
+import clinical.document.shared.Telecom;
+
+public class
+
+
+HomePhone extends Telecom {
+
+    public HomePhone() {
+        this.use = "HP";
+    }
+}
